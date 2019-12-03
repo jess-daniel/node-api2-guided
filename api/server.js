9f0fe463a6ae7229f640f26b97f1cad82ddf1e22
Jess Daniel <jessdaniel474@gmail.com>
2019-12-03 14:07:31 -0500

    added messages to router

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const hubsRouter = require("../hubs/hubs-router");
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+server.use("/api/hubs", hubsRouter);
+
+
+
+module.exports = server;
